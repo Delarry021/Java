79f5a6eaf7f839d308e85dd7021c60ffb7b935e7
Guilherme <81939515+Delarry021@users.noreply.github.com>
2022-07-31 13:33:45 -0300

    Update and rename pokemon.java to Main.java

---
+++ pokemons_objetos/src/br/com/delarry/pokemons/Main.java
@@ -0,0 +1,11 @@
+package br.com.delarry.pokemons;
+
+public class Main {
+    public static void main(String[] args) {
+        Pokemon charmander = new Pokemon();
+
+        charmander.Entrada();
+        charmander.Imprimir();
+
+    }
+}
